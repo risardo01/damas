945f140afac882a6eefbf413347fcc75e4eeefee
risardo01 <risardo01@gmail.com>
2021-04-07 08:10:38 -0500

    initial commit

--- damas.py
+++ damas.py
@@ -1,9 +1,10 @@
 import sys
 
-#funcion pa moverse reyna
-def validation():
+player = 1
 
-	pass
+x = 0
+y = 0
+
 
 class token(object):
 	"""docstring for token"""
@@ -25,6 +26,17 @@ class queen(token):
 	def moveback():
 		pass
 
+def validation(ubic):
+	cantBeUsed = False
+
+	if ToUication = 0:
+		cantBeUsed = True	
+	else:
+		cantBeUsed = False
+	return cantBeUsed
+	pass
+
+
 
 class tab(object):
 	"""docstring for tab"""
@@ -32,33 +44,164 @@ class tab(object):
 		super(tab, self).__init__()
 		self.arg = arg	
 	#tabla
-	tablero = [[0]*8 for i in range(8)]
+	tablero0 = [[0]for i in range(8)]
+	tablero1 = [[0]for i in range(8)]
+	tablero2 = [[0]for i in range(8)]
+	tablero3 = [[0]for i in range(8)]
+	tablero4 = [[0]for i in range(8)]
+	tablero5 = [[0]for i in range(8)]
+	tablero6 = [[0]for i in range(8)]
+	tablero7 = [[0]for i in range(8)]
 
+	tablero0y = [[0]for i in range(8)]
+	tablero1y = [[0]for i in range(8)]
+	tablero2y = [[0]for i in range(8)]
+	tablero3y = [[0]for i in range(8)]
+	tablero4y = [[0]for i in range(8)]
+	tablero5y = [[0]for i in range(8)]
+	tablero6y = [[0]for i in range(8)]
+	tablero7y = [[0]for i in range(8)]
+
+	tablero0[1] = '1'
+	tablero0[3] = '1'
+	tablero0[5] = '1'
+	tablero0[7] = '1'
+
+	tablero1[0] = '1'
+	tablero1[2] = '1'
+	tablero1[4] = '1'
+	tablero1[6] = '1'
+
+	tablero2[1] = '1'
+	tablero2[3] = '1'
+	tablero2[5] = '1'
+	tablero2[7] = '1'
+
+	tablero3[0] = '1'
+	tablero3[2] = '1'
+	tablero3[4] = '1'
+	tablero3[6] = '1'
+
+	tablero4[1] = '1'
+	tablero4[3] = '1'
+	tablero4[5] = '1'
+	tablero4[7] = '1'
+
+	tablero5[0] = '1'
+	tablero5[2] = '1'
+	tablero5[4] = '1'
+	tablero5[6] = '1'
+
+	tablero6[1] = '1'
+	tablero6[3] = '1'
+	tablero6[5] = '1'
+	tablero6[7] = '1'
+
+	tablero7[0] = '1'
+	tablero7[2] = '1'
+	tablero7[4] = '1'
+	tablero7[6] = '1'
+	#◻ ■ ⬤
+	#tablero
+
+	tablero0[0] = '⬤'
+	tablero0[2] = '⬤'
+	tablero0[4] = '⬤'
+	tablero0[6] = '⬤'
+
+	tablero1[1] = '⬤'
+	tablero1[3] = '⬤'
+	tablero1[5] = '⬤'
+	tablero1[7] = '⬤'
+
+
+
+	tablero6[0] = '⬤'
+	tablero6[2] = '⬤'
+	tablero6[4] = '⬤'
+	tablero6[6] = '⬤'
+
+	tablero7[1] = '⬤'
+	tablero7[3] = '⬤'
+	tablero7[5] = '⬤'
+	tablero7[7] = '⬤'
+
+
+	#lado y
+	tablero0y[0] = tablero0[0]
+	tablero0y[6] = tablero6[0]
+
+	tablero1y[1] = tablero1[1]
+	tablero1y[7] = tablero7[1]
+
+	tablero2y[0] = tablero0[2]
+	tablero2y[6] = tablero6[2]
+
+	tablero3y[1] = tablero1[3]
+	tablero3y[7] = tablero7[3]
+
+	tablero4y[0] = tablero0[4]
+	tablero4y[6] = tablero6[4]
+
+	tablero5y[1] = tablero1[5]
+	tablero5y[7] = tablero7[5]
+
+	tablero6y[0] = tablero0[6]
+	tablero6y[6] = tablero6[6]
+
+	tablero7y[1] = tablero1[7]
+	tablero7y[7] = tablero7[7]
+
+	
+	print (tablero7)
+	print (tablero6)
+	print (tablero5)
+	print (tablero4)
+	print (tablero3)
+	print (tablero2)
+	print (tablero1)
+	print (tablero0)
+
+
+
+
+class coord(object):
+	"""docstring for coord"""
+	def __init__(self, arg):
+		super(coord, self).__init__()
+		self.arg = arg
+	def tabUbication():
+		
+		pass
+		
 movimientoValido = False
 
 
+def turns():
+		
+	print("Player",player,"turn")
 
-
-inx = input("Enter the x coord")
-x = int(inx)
-if (x > 7) or (x < 0):
-	sys.exit("Numero invalido")
+	inx = input("Enter the x coord")
+	x = int(inx)
+	if (x > 7) or (x < 0):
+		sys.exit("Numero invalido")
 	
 
-iny = input("Enter the y coord")
-y = int(iny)
-if (y > 7) or (y < 0):
-	sys.exit("Numero invalido")
+	iny = input("Enter the y coord")
+	y = int(iny)
+	if (y > 7) or (y < 0):
+		sys.exit("Numero invalido")
 	
-print ("To")
+	print ("To")
 
-tox = input("Enter the x coord")
-x = int(inx)
-if (x > 7) or (x < 0):
-	sys.exit("Numero invalido")
+	tox = input("Enter the x coord")
+	x = int(inx)
+	if (x > 7) or (x < 0):
+		sys.exit("Numero invalido")
 	
 
-toy = input("Enter the y coord")
-y = int(iny)
-if (y > 7) or (y < 0):
-	sys.exit("Numero invalido")
+	toy = input("Enter the y coord")
+	y = int(iny)
+	if (y > 7) or (y < 0):
+		sys.exit("Numero invalido")
+	pass
